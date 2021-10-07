87a2266ae1a50e07bb725022c90313849a91f9b2
Satya <satyaganesh987@gmail.com>
2021-10-07 13:16:58 +0530

    added Interpolation

--- one-Angular-beggining/src/app/app.component.ts
+++ one-Angular-beggining/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // property of app component
   title = 'one-Angular-beggining';
+  myName = "Satya";
+
+  showWelcomeMessage() {
+    return "Welcome ";
+  }
 }
