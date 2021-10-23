81045f7dcea847d9281c8a33de4b2cc2551fa466
Satya <satyaganesh987@gmail.com>
2021-10-23 09:56:17 +0530

    Added buildin Pipes and custom pipes

---
+++ Pipes/src/app/app.service.ts
@@ -0,0 +1,12 @@
+import {Pipe, PipeTransform } from '@angular/core'
+@Pipe({
+    name : "summary"
+})
+export class CustomPipe implements PipeTransform {
+    transform(value: string, ...args: any[]) {
+        if(!value) {
+            return null;
+        }
+        return value.substr(0,50)  + "...";
+    }  
+}
